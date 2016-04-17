06bdf322721718a793a8d8f70971d702d6ef9639
Reuben Tyrrell Castelino <rtcastelino@ucdavis.edu>
2016-04-17 13:24:36 -0700

    implemented hello

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <unistd.h>
 
 #include "VirtualMachine.h"
 
 using namespace std;
 
-TVMStatus VMStart(int tickms, int argc, char *argv[])
-{
-  return VM_STATUS_SUCCESS;
-}
+extern "C" {
 
-TVMStatus VMFileWrite(int filedescriptor, void *data, int *length)
-{ 
-  if (*length == NULL)
-    return VM_STATUS_ERROR_INVALID_PARAMETER;
-  return VM_STATUS_SUCCESS;
-}
+  TVMMainEntry VMLoadModule(const char *module);
 
+  TVMStatus VMStart(int tickms, int argc, char *argv[])
+  {
+    string module_name(argv[0]);
+    TVMMainEntry main_entry = VMLoadModule(module_name.c_str());
+
+    string s = "module: " + module_name + "\n";
+    VMPrint(s.c_str());
+    VMFilePrint(1, s.c_str());
+
+    main_entry(argc, argv);
+
+    return VM_STATUS_SUCCESS;
+  }
+
+  TVMStatus VMFileWrite(int filedescriptor, void *data, int *length)
+  { 
+    if (*length == NULL)
+      return VM_STATUS_ERROR_INVALID_PARAMETER;
+
+    write(filedescriptor, data, *length);
+    return VM_STATUS_SUCCESS;
+  }
+
+  TVMStatus VMThreadSleep(TVMTick tick)
+  {
+  }
+
+}
